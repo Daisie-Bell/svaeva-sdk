47cba535db5498cd2d808d74e560f94171346749
vortex <vortex@debian.lan>
2023-10-13 13:53:06 +0100

    Set Endpoint and key on call added!

--- svaeva/__init__.py
+++ svaeva/__init__.py
@@ -6,8 +6,11 @@ from svaeva.Paths.MultiAPI.Skeletrons import Skeleton
 
 class Panel:
 
-    def __init__(self):
-        self.client = Client()
+    def __init__(self,url="",token=""):
+        if url != "" and token != "":
+            self.client = Client(url=url,key=token)
+        else:
+            self.client = Client()
         self.platform = Platform(self.client)
         self.group    = Group(self.client)
         self.skeleton = Skeleton(self.client)
